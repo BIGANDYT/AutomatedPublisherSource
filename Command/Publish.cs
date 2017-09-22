94c31082041e30e0f2c03eea7ecbc182a5b2ecea
ISC2\rguyett <rguyett@isc2.org>
2017-09-22 15:50:05 -0400

    updated taskbuilder to be more precise, refactored some code, add async support and tasks now automatically remove themselves when they complete

--- Command/Publish.cs
+++ Command/Publish.cs
@@ -4,6 +4,9 @@ using Sitecore.Data;
 using Sitecore.Publishing;
 using Sitecore.SecurityModel;
 using Sitecore.Diagnostics;
+using System.Linq;
+using Sitecore.Globalization;
+using Sitecore.Configuration;
 
 namespace Sitecore.Modules.AutomatedPublisher.Command
 {
@@ -21,112 +24,132 @@ namespace Sitecore.Modules.AutomatedPublisher.Command
     /// </remarks>
     /// <copyright>Copyright © 2010-2011 Joel Konecny and David DeBruin Sitecore Shared Source Modules</copyright>
     [CLSCompliant(false)]
-	public class Publish
-	{
-		/// <summary>
-		/// This method gets called from a scheduled task command.
-		/// You define a scheduled task command by navigating to 
-		/// /System/Tasks/Commands and inserting a new command
-		/// </summary>
-		/// <param name="items">Items to publish</param>
-		/// <param name="command">Command Info</param>
-		/// <param name="schedule">Schedule Item</param>
-		public void Run(Item[] items, Tasks.CommandItem command, Tasks.ScheduleItem schedule)
-		{
-			DateTime dtPublishDate = DateTime.UtcNow;
+    public class Publish
+    {
+        private Database _master = null;
+        protected Database master
+        {
+            get
+            {
+                return _master ?? (_master = Sitecore.Configuration.Factory.GetDatabase(Constants.MasterDatabaseName));
+            }
+        }
+        /// <summary>
+        /// This method gets called from a scheduled task command.
+        /// You define a scheduled task command by navigating to 
+        /// /System/Tasks/Commands and inserting a new command
+        /// </summary>
+        /// <param name="items">Items to publish</param>
+        /// <param name="command">Command Info</param>
+        /// <param name="schedule">Schedule Item</param>
+        public void Run(Item[] items, Tasks.CommandItem command, Tasks.ScheduleItem schedule)
+        {
+            //DateTime dtPublishDate = DateTime.UtcNow;
 
-			using (new SecurityDisabler())
-			{
-				try
-				{
-					Database master = Sitecore.Configuration.Factory.GetDatabase(Constants.MasterDatabaseName);
-					Sitecore.Globalization.Language[] languages = master.Languages;
+            using (new SecurityDisabler())
+            {
+                try
+                {
+                    // for each existing target
+                    Sitecore.Collections.ChildList publishingTargets = master.GetItem(Constants.PublishingTargetsFolder)?.Children;
+                    foreach (Item publishingTarget in publishingTargets)
+                    {
+                        var targetDatabase = GetTargetDatabase(publishingTarget);
+                        if(targetDatabase == null)
+                        {
+                            Log.Info("Skipping target: Could not find Database target for Publishing target: " + publishingTarget.Name, this);
+                            continue;
+                        }
 
-					// for each existing target
-					Item publishingTargetsFolder = master.GetItem(Constants.PublishingTargetsFolder);
-					Sitecore.Collections.ChildList publishingTargets = publishingTargetsFolder.Children;
-					foreach (Item publishingTarget in publishingTargets)
-					{
-						if (publishingTarget != null)
-						{
-							string targetDBName = publishingTarget[Constants.TargetDatabase];
-							if (string.IsNullOrEmpty(targetDBName) == false)
-							{
-								Database targetDb = Sitecore.Configuration.Factory.GetDatabase(targetDBName);
-								if (targetDb != null)
-								{
-									// for each existing language
-									foreach (Sitecore.Globalization.Language language in languages)
-									{
-										foreach (Item actItem in items)
-										{
-											ID itemToPublishId = actItem.ID;
-											Item itemToPublish = master.GetItem(itemToPublishId, language);
-											bool bPublish = false;
-
-											Sitecore.Data.Fields.MultilistField targets = itemToPublish.Fields[Sitecore.FieldIDs.PublishingTargets];
-											if (targets != null)
-											{
-												Item[] itemToPublishTargets = targets.GetItems();
-												if (itemToPublishTargets.Length > 0)
-												{
-													foreach (Item itemToPublishTarget in itemToPublishTargets)
-													{
-														if (string.Equals(itemToPublishTarget.Name, publishingTarget.Name, StringComparison.CurrentCultureIgnoreCase) == true)
-														{
-															bPublish = true;
-															break;
-														}
-													}
-												}
-												else
-												{
-													bPublish = true;
-												}
-											}
-											else
-											{
-												bPublish = true;
-											}
-
-											if (bPublish == true)
-											{
-												try
-												{
-													string strMessage = string.Format(System.Globalization.CultureInfo.CurrentCulture, "Item '{0}' Target '{1}' Language '{2}': Automated Publisher Schedule Item '{3}'", itemToPublish.ID.ToString(), publishingTarget.Name, language.Name, schedule.Name);
-													Log.Info(strMessage, this);
-												}
-												catch
-												{
-                                                    Log.Error(Constants.ErrorFormattingString, this);
-                                                }
-
-												PublishOptions publishOptions = new PublishOptions(master, targetDb, PublishMode.Full, language, dtPublishDate);
-												publishOptions.Deep = true;
-												publishOptions.RootItem = itemToPublish;
-
-												Publisher publisher = new Publisher(publishOptions);
-												publisher.Publish();
-											}
-											else
-											{
-												string strMessage = string.Format(System.Globalization.CultureInfo.CurrentCulture, "Item '{0}' Target '{1}' Language '{2}': No publishing targets were selected in Automated Publisher",
-													itemToPublish.ID.ToString(), publishingTarget.Name, language.Name);
-												Log.Error(strMessage, this);
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-					schedule.Remove();					
+                        PublishItems(items, publishingTarget, targetDatabase);
+                    }
                 }
-				catch (Exception ex)
-				{
-					Log.Error("Error Publishing in Automated Publisher", ex, this);
-				}
-			}
-		}
-	}
+                catch (Exception ex)
+                {
+                    Log.Error("Error Publishing in Automated Publisher", ex, this);
+                }
+            }
+        }
+
+        protected void PublishItems(Item[] items, Item publishingTarget, Database targetDb)
+        {
+            
+
+            // for each existing language
+            foreach (var language in master.Languages)
+            {
+                foreach (Item actItem in items)
+                {
+                    ID itemToPublishId = actItem.ID;
+                    Item itemToPublish = master.GetItem(itemToPublishId, language);
+
+                    try
+                    {
+                        if (ValidatePublishingTargets(itemToPublish, publishingTarget))
+                        {
+                            string strMessage = string.Format(System.Globalization.CultureInfo.CurrentCulture, "Automated Publishing Item '{0}' Target '{1}' Language '{2}'", itemToPublish.ID.ToString(), publishingTarget.Name, language.Name);
+                            Log.Info(strMessage, this);
+
+
+                            PublishItem(itemToPublish, targetDb, language);
+                        }
+                        else
+                        {
+                            string strMessage = string.Format(System.Globalization.CultureInfo.CurrentCulture, "Automated Publishing Item '{0}' Target '{1}' Language '{2}': No publishing targets were selected in Automated Publisher",
+                                itemToPublish.ID.ToString(), publishingTarget.Name, language.Name);
+                            throw new Exception(strMessage);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error("Error Auto publishing for Item: " + actItem.ID + ", Language: " + language.Name, e, this);
+                    }
+                }
+            }
+        }
+
+        protected bool ValidatePublishingTargets(Item itemToPublish, Item publishingTarget)
+        {
+            Sitecore.Data.Fields.MultilistField targets = itemToPublish.Fields[Sitecore.FieldIDs.PublishingTargets];
+
+            var itemToPublishTargets = targets?.GetItems();
+            if (itemToPublishTargets != null)
+            {
+                if (itemToPublishTargets.Any(x => string.Equals(x.Name, publishingTarget.Name)))
+                {
+                    return true; //found matching target!
+                }
+            }
+            else
+            {
+                //there are no available publishing targets to select so no target is still valid
+                return true;
+            }
+
+            //couldnt find a matching target from the available targets
+            return false;
+
+        }
+
+        protected Database GetTargetDatabase(Item publishingTarget)
+        {
+            string targetDBName = publishingTarget[Constants.TargetDatabase];
+            if (string.IsNullOrWhiteSpace(targetDBName))
+            {
+                return null;
+            }
+
+            return Factory.GetDatabase(targetDBName);
+        }
+
+        protected void PublishItem(Item itemToPublish, Database targetDb, Language language)
+        {
+            PublishOptions publishOptions = new PublishOptions(master, targetDb, PublishMode.Full, language, DateTime.UtcNow);
+            publishOptions.Deep = true;
+            publishOptions.RootItem = itemToPublish;
+
+            Publisher publisher = new Publisher(publishOptions);
+            publisher.Publish();
+        }
+    }
 }
